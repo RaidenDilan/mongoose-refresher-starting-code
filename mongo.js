78dd5b750f585d3be688a006e3fa859d37cb3deb
RaidenDilan <raiden18@me.com>
2020-05-01 01:09:16 +0100

    Creating a Simple Backend & Connecting it to the Database + Creating a Document with MongoDB

---
+++ mongo.js
@@ -0,0 +1,34 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = `mongodb+srv://${process.env.MONGO_CLIENT_USERNAME}:${process.env.MONGO_CLIENT_PASSWORD}@cluster0-jxlm3.mongodb.net/products_test?retryWrites=true&w=majority`;
+
+MongoClient.connect(url);
+
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price
+  };
+
+  const client = new MongoClient(url);
+
+  try {
+    await client.connect();
+    const db = client.db();
+    const result = db.collection('products').insertOne(newProduct);
+  }
+  catch(err) {
+    // console.log('err', err);
+    return res.json({ message: 'Could not store Data!', err });
+  }
+
+  client.close();
+
+  res.json(newProduct);
+};
+
+const getProducts = async (req, res, next) => {
+
+};
+
+module.exports = { createProduct, getProducts };
